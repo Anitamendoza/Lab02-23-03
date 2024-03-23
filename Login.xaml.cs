f05c9647f7ad6a87a80568b665358cc456752488
Mendoza Ana <95864327+Anitamendoza@users.noreply.github.com>
2024-03-23 16:20:17 -0500

    Add files via upload

---
+++ Login.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsuario.Text;
+            string password = txtPassword.Password;
+
+            bool isValid = ValidateUser(username, password);
+
+            if (isValid)
+            {
+                Menu menu = new Menu();
+                menu.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos. Por favor, inténtelo de nuevo.", "Error de inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private bool ValidateUser(string username, string password)
+        {
+            return username == "Mendoza" && password == "1234";
+        }
+
+    }
+}
